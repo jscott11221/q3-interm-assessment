1b2ebcc6ca0e490702da8b60430b4746811dfe7f
john <jscott@wlapcs.org>
2017-04-11 17:31:30 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,28 @@
+function setup(){
+    createCanvas(600,400);
+    background(66, 173, 244);
+}
+
+function draw(){
+    circle(300,200);
+    circle(300,200);
+    circle(300,200);
+    circle(300,200);
+    circle(300,200);
+    
+    
+function circle(x,y){
+    fill(300,200);
+    rect();
+     
+    
+ fill(255,150,200);
+ Stroke(50);
+ StrokeWeight();
+ ellipse(10,10,35,35);
+ ellipse(10,10,35,35);
+ ellipse(10,10,35,35);
+ ellipse(10,10,35,35);
+ 
+ if(mousedIspressed){
+     //change the background color to red
